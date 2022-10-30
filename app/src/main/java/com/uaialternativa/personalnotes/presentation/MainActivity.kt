6b1ac47f2ae4c8ad70ff78fc948361c2a02bee1a
fabiocunha <fxbrasil@gmail.com>
2022-10-30 16:25:53 -0300

    up

--- app/src/main/java/com/uaialternativa/personalnotes/presentation/MainActivity.kt
+++ app/src/main/java/com/uaialternativa/personalnotes/presentation/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.uaialternativa.personalnotes.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+//import com.sanojpunchihewa.updatemanager.UpdateManager
+//import com.sanojpunchihewa.updatemanager.UpdateManagerConstant
 import com.uaialternativa.personalnotes.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    //third-party library - https://github.com/SanojPunchihewa/InAppUpdater
+    //lateinit var mUpdateManager: UpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         println("app start main activity ___")
+
+        // Initialize the Update Manager with the Activity and the Update Mode
+        //mUpdateManager = UpdateManager.Builder(this).mode(UpdateManagerConstant.IMMEDIATE);
+        //mUpdateManager.start();
+
     }
+
+
 }
--- app/src/main/java/com/uaialternativa/personalnotes/util/AppUtil.kt
+++
@@ -1,8 +0,0 @@
-package com.uaialternativa.personalnotes.util
-
-import android.R
-
-class AppUtil {
-
-
-}
